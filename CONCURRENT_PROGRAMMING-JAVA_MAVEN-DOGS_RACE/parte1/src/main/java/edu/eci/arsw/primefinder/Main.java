8c887922c43a2194fc337c9a16ae23f7be768483
Daniel Varon <daniel.varon@mail.escuelaing.edu.co>
2021-01-27 12:03:04 -0500

    Punto 1

--- CONCURRENT_PROGRAMMING-JAVA_MAVEN-DOGS_RACE/parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ CONCURRENT_PROGRAMMING-JAVA_MAVEN-DOGS_RACE/parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -4,59 +4,48 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-
-	private static ArrayList<PrimeFinderThread> hilos= new ArrayList<PrimeFinderThread>();
-	public static void main(String[] args) throws InterruptedException {
-		int numeroHilos=1;
-		int cotaMenor= 0;
-		int cotaMayor= 30000000;
-		int inicio=0;
-		int cota=(int) Math.ceil(30000000/numeroHilos)-1;
-		int fin=cota;
-
-		for (int i=0; i<numeroHilos;i++) {
-			PrimeFinderThread pft = new PrimeFinderThread(inicio, fin);
-			hilos.add(pft);
-			inicio=fin+1;
-			if(fin<cotaMayor-cota && cotaMayor-cota<fin+cota) fin=cotaMayor;
-			else if(fin+cota<cotaMayor) fin=fin+cota;
-			else fin=cotaMayor;
-
-
-
-		}
-		for(PrimeFinderThread x:hilos){x.start();}
-			Thread.currentThread().sleep(5000);
-
-
-			for (PrimeFinderThread ini:hilos){
-				ini.suspend();
-				System.out.println("me suspendi");
-				ini.start();
-			}
-			//pulsar enter
-
-		System.out.println("here");
-		Scanner scanner = new Scanner(System.in);
-		String readString = scanner.nextLine();
-		while(readString!=null) {
-			System.out.println(readString);
-
-			if (readString.isEmpty()) {
-				System.out.println("Read Enter Key.");
-			}
-
-			if (scanner.hasNextLine()) {
-				readString = scanner.nextLine();
-			} else {
-				readString = null;
-			}
-		}
-		for (PrimeFinderThread e:hilos){
-			e.resume();
-
-		}
-
-		}
-	
+    
+    private static ArrayList<PrimeFinderThread> hilos= new ArrayList<PrimeFinderThread>();
+    
+    public static void main(String[] args) throws InterruptedException {
+        int numeroHilos=3;
+        int cotaMayor= 30000000;
+        int inicio=0;
+        int cota=(int) Math.ceil(30000000/numeroHilos);
+        int fin=cota-1;
+        
+        for (int i=0; i<numeroHilos;i++) {
+            PrimeFinderThread pft = new PrimeFinderThread(inicio, fin);
+            System.out.println(inicio+" aqui vamos "+fin);
+            hilos.add(pft);
+            inicio=fin+1;
+            if(fin<cotaMayor-cota && cotaMayor-cota<fin+cota) fin=cotaMayor;
+            else if(fin+cota<cotaMayor) fin=fin+cota;
+            else fin=cotaMayor;
+        }
+        
+        hilos.forEach(x -> { x.start();});
+        
+        Thread.sleep(5000);
+        hilos.forEach(ini -> {ini.suspenderHilo();});
+        
+        pressAnyKeyToContinue();
+        
+        hilos.forEach(ini -> {ini.renaudarHilo();});
+        
+    }
+    
+    static public void pressAnyKeyToContinue()
+    {
+        String seguir;
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Press Enter key to continue...");
+        try
+        {
+            seguir = teclado.nextLine();
+        }
+        catch(Exception e)
+        {}
+    }
+    
 }
--- CONCURRENT_PROGRAMMING-JAVA_MAVEN-DOGS_RACE/parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ CONCURRENT_PROGRAMMING-JAVA_MAVEN-DOGS_RACE/parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -2,45 +2,72 @@ package edu.eci.arsw.primefinder;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PrimeFinderThread extends Thread{
-
-	
-	int a,b;
-	
-	private List<Integer> primes=new LinkedList<Integer>();
-	
-	public PrimeFinderThread(int a, int b) {
-		super();
-		this.a = a;
-		this.b = b;
-	}
-
-	public void run(){
-		for (int i=a;i<=b;i++){						
-			if (isPrime(i)){
-				primes.add(i);
-				System.out.println(i);
-			}
-		}
-		
-		
-	}
-	
-	boolean isPrime(int n) {
-	    if (n%2==0) return false;
-	    for(int i=3;i*i<=n;i+=2) {
-	        if(n%i==0)
-	            return false;
-	    }
-	    return true;
-	}
-
-	public List<Integer> getPrimes() {
-		return primes;
-	}
-	
-	
-	
-	
+    
+    boolean suspender; //Suspende un hilo cuando es true
+    boolean pausar;    //Detiene un hilo cuando es true
+    int a,b;
+    
+    private List<Integer> primes=new LinkedList<Integer>();
+    
+    public PrimeFinderThread(int a, int b) {
+        super();
+        this.a = a;
+        this.b = b;
+        suspender=false;
+        pausar=false;
+    }
+    
+    @Override
+    public void run(){
+        for (int i=a;i<=b;i++){
+            if (isPrime(i)){
+                primes.add(i);
+                System.out.println(i);
+            }
+            synchronized (this) {
+                while (suspender) {
+                    try {
+                        wait();
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(PrimeFinderThread.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                if (pausar) break;
+            }
+        }
+    }
+    
+    boolean isPrime(int n) {
+        if (n%2==0) return false;
+        for(int i=3;i*i<=n;i+=2) {
+            if(n%i==0)
+                return false;
+        }
+        return true;
+    }
+    
+    public List<Integer> getPrimes() {
+        return primes;
+    }
+    
+    //Pausar el hilo
+    synchronized void pausarHilo(){
+        pausar=true;
+        //lo siguiente garantiza que un hilo suspendido puede detenerse.
+        suspender=false;
+        notify();
+    }
+    //Suspender un hilo
+    synchronized void suspenderHilo(){
+        suspender=true;
+    }
+    //Renaudar un hilo
+    synchronized void renaudarHilo(){
+        suspender=false;
+        notify();
+    }
 }
